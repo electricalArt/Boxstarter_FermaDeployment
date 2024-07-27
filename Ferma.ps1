261c82506cced41769c82a893e94ee6c2bf5da1c
Artur <58558002+elektrikArt@users.noreply.github.com>
2024-07-27 05:56:39 -0700

    Update Ferma.ps1

--- Ferma.ps1
+++ Ferma.ps1
@@ -9,9 +9,14 @@ choco install winget
 $wingetPath = "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_1.23.1911.0_x64__8wekyb3d8bbwe\"
 [System.Environment]::SetEnvironmentVariable("Path", $env:Path + $wingetPath, [System.EnvironmentVariableTarget]::Machine)
 #>
-$winget_release = "https://github.com/microsoft/winget-cli/releases/download/v1.0.11451/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle"
-Invoke-WebRequest -Uri $winget_release -OutFile ~/Downloads/Microsoft.DesktopAppInstaller.appxbundle -UseBasicParsing
-Add-AppxPackage -Path ~/Downloads/Microsoft.DesktopAppInstaller.appxbundle
+$progressPreference = 'silentlyContinue'
+Write-Information "Downloading WinGet and its dependencies..."
+Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx
+Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 <#
 choco install `
     vim `
