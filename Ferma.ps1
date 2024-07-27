bde2a160f2cb863a4557df902604ccd84294413d
Artur <58558002+elektrikArt@users.noreply.github.com>
2024-07-27 05:40:13 -0700

    Update Ferma.ps1

--- Ferma.ps1
+++ Ferma.ps1
@@ -26,8 +26,14 @@ Set-BoxstarterTaskbarOptions -DisableSearchBox
 Disable-GameBarTips
 Disable-BingSearch
 
-# Use `winget` as standard package manager
+<#
 choco install winget
+$wingetPath = "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_1.23.1911.0_x64__8wekyb3d8bbwe\"
+[System.Environment]::SetEnvironmentVariable("Path", $env:Path + $wingetPath, [System.EnvironmentVariableTarget]::Machine)
+#>
+$winget_release = "https://github.com/microsoft/winget-cli/releases/download/v1.0.11451/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle"
+Invoke-WebRequest -Uri $winget_release -OutFile ~/Downloads/Microsoft.DesktopAppInstaller.appxbundle -UseBasicParsing
+Add-AppxPackage -Path ~/Downloads/Microsoft.DesktopAppInstaller.appxbundle
 <#
 choco install `
     vim `
@@ -42,6 +48,7 @@ choco install `
     discord `
 #>
 
+# Use choco as default package manager (it has abitliy to specify packages installation directory
 winget install --silent `
 	Vim.Vim `
 	Alacritty.Alacritty `
