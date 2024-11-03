823002cae2b1d71ac37efd74ab58f5260940bd89
artur <muslimow9@yandex.ru>
2024-11-03 23:34:31 +0300

    Update

--- 1_Ferma.ps1
+++ 1_Ferma.ps1
@@ -28,7 +28,8 @@ function InstallNewWinget()
     if (!(Get-Command "winget" -ErrorAction "SilentlyContinue"))
     {
         $progressPreference = 'silentlyContinue'
-        Write-Information "Downloading WinGet and its dependencies..."
+        Write-BoxstarterMessage `
+            -Message ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Downloading WinGet and its dependencies..."
         Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
         Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
         Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
