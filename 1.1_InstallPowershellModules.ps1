06b733d7cfd764a2def7f91d42313085dbe93558
Artur <58558002+electricalArt@users.noreply.github.com>
2026-05-31 18:47:25 +0300

    Update

--- 1.1_InstallPowershellModules.ps1
+++ 1.1_InstallPowershellModules.ps1
@@ -1,5 +1,9 @@
 function InstallPowershellModules()
 {
+    # To skip confirmation (****didn't tested)
+    Install-PackageProvider NuGet -Force
+    Set-PSRepository PSGallery -InstallationPolicy Trusted
+    
     Install-Module -Name PSTree -Confirm
     Install-Module -Name MagicPacket -Confirm
 
